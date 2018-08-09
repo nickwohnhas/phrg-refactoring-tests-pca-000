19e9f0843036aac8b85045f831b0c8841cbcf1db
Nick Wohnhas <nicholas.wohnhas@powerhrg.com>
2018-08-08 21:01:16 -0400

    more factorybot practice

--- app/models/author.rb
+++ app/models/author.rb
@@ -3,4 +3,12 @@
 class Author < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
   validates :phone_number, length: { is: 10 }
+
+  FactoryBot.define do
+    factory :author do
+      name "H"
+      phone_number "1234567890"
+    end
+  end
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -18,4 +18,13 @@ class Post < ActiveRecord::Base
     return unless CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
     errors.add(:title, "must be clickbait")
   end
+
+  FactoryBot.define do
+    factory :post do
+      title "Secret"
+      content "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dapibus, nulla vel condimentum ornare, arcu lorem hendrerit purus, ac sagittis ipsum nisl nec erat. Morbi porta sollicitudin leo, eu cursus libero posuere ac. Sed ac ultricies ante. Donec nec nulla ipsum. Nunc eleifend, ligula ut volutpat."
+      summary "Awesome"
+      category "Fiction"
+    end
+  end
 end
